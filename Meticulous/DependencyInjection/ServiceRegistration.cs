1ac8b99777043a4ccc6a5fdaee4c312dba882a45
vishaludugade99@gmail.com <vishaludugade99@gmail.com>
2026-08-10 22:44:36 +0530

    working version of navigation service

--- Meticulous/DependencyInjection/ServiceRegistration.cs
+++ Meticulous/DependencyInjection/ServiceRegistration.cs
@@ -1,4 +1,10 @@
 ﻿using Meticulous.Infrastructure.Services.Navigation;
+using Meticulous.Modules.Alerts.ViewModel;
+using Meticulous.Modules.Dashboard.ViewModel;
+using Meticulous.Modules.Notification.ViewModel;
+using Meticulous.Modules.Processes.ViewModel;
+using Meticulous.Modules.Reports.ViewModel;
+using Meticulous.Modules.Settings.ViewModel;
 using Meticulous.ViewModel;
 using Meticulous.ViewModel.Regions;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +18,13 @@ namespace Meticulous.Shell.DependencyInjection
             var services = new ServiceCollection();
 
             // Register dependency services first, then dependent services
+            services.AddSingleton<DashboardViewModel>();
+            services.AddSingleton<AlertsViewModel>();
+            services.AddSingleton<ProcessesViewModel>();
+            services.AddSingleton<ReportsViewModel>();
+            services.AddSingleton<SettingsViewModel>();
+            services.AddSingleton<NotificaitonViewModel>();
+
             services.AddSingleton<INavigationService, NavigationService>();
             services.AddSingleton<NavigationViewModel>();
             services.AddSingleton<ToolbarViewModel>();
--- Meticulous/ViewModel/Regions/NavigationViewModel.cs
+++ Meticulous/ViewModel/Regions/NavigationViewModel.cs
@@ -17,7 +17,11 @@ namespace Meticulous.ViewModel.Regions
 
         public ObservableCollection<NavigationItem> NavigationItems { get; }
 
-        public ICommand NavigateCommand { get; }
+        public ICommand DashboardCommand { get; }
+        public ICommand ProcessesCommand { get; }
+        public ICommand AlertsCommand { get; }
+        public ICommand ReportsCommand { get; }
+        public ICommand SettingsCommand { get; }
 
         public NavigationViewModel(INavigationService navigationService)
         {
@@ -32,136 +36,11 @@ namespace Meticulous.ViewModel.Regions
                 new NavigationItem("Settings", typeof(SettingsViewModel))
             };
 
-            NavigateCommand =
-            new RelayCommand(Navigate);
+            DashboardCommand = new RelayCommand(_ => _navigationService.NavigateTo<DashboardViewModel>());
+            ProcessesCommand = new RelayCommand(_ => _navigationService.NavigateTo<ProcessesViewModel>());
+            AlertsCommand = new RelayCommand(_ => _navigationService.NavigateTo<AlertsViewModel>());
+            ReportsCommand = new RelayCommand(_ => _navigationService.NavigateTo<ReportsViewModel>());
+            SettingsCommand = new RelayCommand(_ => _navigationService.NavigateTo<SettingsViewModel>());
         }
-
-        private void Navigate(object parameter)
-        {
-            if (parameter is NavigationItem item)
-            {
-                _navigationService.NavigateTo(
-                    item.ViewModelType);
-            }
-        }
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //private ICommand _dashboardCommand;
-        //public ICommand DashboardCommand
-        //{
-        //    get
-        //    {
-        //        if (_dashboardCommand == null)
-        //        {
-        //            _dashboardCommand = new RelayCommands(IsCanExecute, ExecuteDashboardCommand);
-        //        }
-        //        return _dashboardCommand; // <- also fix: return the command
-        //    }
-        //}
-
-        //private void ExecuteDashboardCommand(object parameter)
-        //{
-        //    // Implement the logic to navigate to the dashboard view
-        //    // For example, you can use a navigation service or set the current view model
-        //    _navigationService.NavigateTo(NavigationItemType.Dashboard);
-        //}
-
-
-        //private ICommand _processesCommand;
-        //public ICommand ProcessesCommand
-        //{
-        //    get
-        //    {
-        //        if (_processesCommand == null)
-        //        {
-        //            _processesCommand = new RelayCommands(IsCanExecute, ExecuteProcessesCommand);
-        //        }
-        //        return _processesCommand; // <- also fix: return the command
-        //    }
-        //}
-
-        //private void ExecuteProcessesCommand(object parameter)
-        //{
-        //    // Implement the logic to navigate to the processes view
-        //    // For example, you can use a navigation service or set the current view model
-        //    _navigationService.NavigateTo(NavigationItemType.Processes);
-        //}
-
-        //private ICommand _alertsCommand;
-        //public ICommand AlertsCommand
-        //{
-        //    get
-        //    {
-        //        if (_alertsCommand == null)
-        //        {
-        //            _alertsCommand = new RelayCommands(IsCanExecute, ExecuteAlertsCommand);
-        //        }
-        //        return _alertsCommand; // <- also fix: return the command
-        //    }
-        //}
-
-        //private void ExecuteAlertsCommand(object parameter)
-        //{
-        //    // Implement the logic to navigate to the alerts view
-        //    // For example, you can use a navigation service or set the current view model
-        //    _navigationService.NavigateTo(NavigationItemType.Alerts);
-        //}
-
-
-        //private ICommand _ReportsCommand;
-        //public ICommand ReportsCommand
-        //{
-        //    get
-        //    {
-        //        if (_ReportsCommand == null)
-        //        {
-        //            _ReportsCommand = new RelayCommands(IsCanExecute, ExecuteReportsCommand);
-        //        }
-        //        return _ReportsCommand; // <- also fix: return the command
-        //    }
-        //}
-
-        //private void ExecuteReportsCommand(object parameter)
-        //{
-        //    // Implement the logic to navigate to the reports view
-        //    // For example, you can use a navigation service or set the current view model
-        //    _navigationService.NavigateTo(NavigationItemType.Reports);
-        //}
-
-
-        //private ICommand _SettingsCommand;
-        //public ICommand SettingsCommand
-        //{
-        //    get
-        //    {
-        //        if (_SettingsCommand == null)
-        //        {
-        //            _SettingsCommand = new RelayCommands(IsCanExecute, ExecuteSettingsCommand);
-        //        }
-        //        return _SettingsCommand; // <- also fix: return the command
-        //    }
-        //}
-
-        //private void ExecuteSettingsCommand(object parameter)
-        //{
-        //    // Implement the logic to navigate to the settings view
-        //    // For example, you can use a navigation service or set the current view model
-        //    _navigationService.NavigateTo(NavigationItemType.Settings);
-        //}
-
-        //private void ExecuteNavigate(object parameter)
-        //{
-        //    if (parameter is NavigationItem item)
-        //    {
-        //        // use the NavigationItem.ViewModelType when navigating
-        //        _navigationService.NavigateTo(item.ViewModelType);
-        //    }
-        //}
-
-        //private bool IsCanExecute(object parameter)
-        //{
-        //    return true;
-        //}
     }
 }
